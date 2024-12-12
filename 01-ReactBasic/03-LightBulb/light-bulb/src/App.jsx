d963e0d512bf12a0cf7383e01e033d9d95c29b61
ABHISHEK <arunabhishek274001@gmail.com>
2024-12-12 21:10:39 +0530

    learn-rolling-up-context-api

---
+++ 01-ReactBasic/03-LightBulb/light-bulb/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState , createContext, useContext } from "react";
+
+
+const BulbContext = createContext();
+
+function App() {
+  const [on , setOn] = useState(false);
+
+  return (
+    <div>
+      <BulbContext.Provider value={{
+        on: on,
+        setOn: setOn
+      }}>
+        <LightBulb />
+      </BulbContext.Provider>
+    </div>
+  )
+}
+function LightBulb(){
+  return (
+    <div>
+      <BulbState />
+      <ToggleBulbState />
+    </div>
+  );
+} 
+
+function BulbState(){
+  const {on} = useContext(BulbContext);
+  return(
+    <div>
+      {on ? "Bulb On" : "Bulb off"}
+    </div>
+  );
+}
+
+function ToggleBulbState(){
+  const {setOn} = useContext(BulbContext);
+  function toggel(){
+    setOn(cs => !cs);
+  }
+  return(
+    <div>
+      <button onClick={toggel} >Toggle the Bulb</button>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
